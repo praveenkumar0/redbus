90663ebf162137c21df372406caee5e3b07180ef
praveenkumar0 <rebal.praveen9901@gmail.com>
2017-07-12 14:34:05 +0530

    Update first.java

--- first.java
+++ first.java
@@ -3,7 +3,8 @@ Class first
 	public static void main(string[args])
 		
 			{
-			System.Out.Println("welcom to git world  .....");
+			System.Out.Println("welcome to git world  .....");
+			System.Out.Println("editing from git-hub  .....");
 			System.Out.Println("modified  .....");
 			}			
 
